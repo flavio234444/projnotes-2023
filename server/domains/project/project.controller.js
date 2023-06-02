388be3dedf63a82e42da350afdffcd9b0f04b9c0
flavio234444 <124796193+flavio234444@users.noreply.github.com>
2023-06-02 13:52:12 +0000

    procesando  formulario

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,17 +1,18 @@
-// Actions methods
-// GET "/project"
-const showDashboard = (req, res) => {
-    res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
-  };
-  
-  // GET "/project/add"
-  const add = (req, res) => {
-    res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
-  };
-  
-  // Controlador user
-  export default {
-    // Action Methods
-    showDashboard,
-    add,
-  }.
+...
+// GET "/project/add"
+const add = (req, res) => {
+  res.render('project/addView');
+};
+
+// POST "/project/add"
+const addPost = (req, res) => {
+
+};
+
+// Controlador user
+export default {
+  // Action Methods
+  showDashboard,
+  add,
+  addPost,
+};
